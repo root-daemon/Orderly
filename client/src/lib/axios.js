830a5f1ad70d9bce0a1ff8d94933c5579a489abf
suhayb <iamsuhaybahmed@gmail.com>
2025-01-30 10:08:50 +0530

    fix: ngrok header client

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_PUBLIC_SERVER_URL,
   withCredentials: true,
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 export default axiosInstance;
