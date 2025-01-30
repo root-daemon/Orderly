127c3868444b801e01e214d0775feaf1b9d66a5b
suhayb <iamsuhaybahmed@gmail.com>
2025-01-31 04:48:36 +0530

    fix: create event date time

--- server/src/utils/timeConverter.js
+++ server/src/utils/timeConverter.js
@@ -1,6 +1,9 @@
-const createEventDateTime = (sessionStart, timeZoneOffset = "+05:30") => {
-  const currentDate = new Date().toISOString().split("T")[0];
-  return `${currentDate}T${sessionStart}:00${timeZoneOffset}`;
+import { DateTime } from "luxon";
+
+const createEventDateTime = (sessionStart, timeZone = "Asia/Kolkata") => {
+  const currentDate = DateTime.now().setZone(timeZone).toFormat("yyyy-MM-dd");
+
+  return `${currentDate}T${sessionStart}:00+05:30`;
 };
 
 export default createEventDateTime;
